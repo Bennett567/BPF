25fb9ff6d0c8f57b2e943a817ad89dab3ae0625e
Bennett567 <dani.bohm1234@gmail.com>
2020-07-02 13:37:15 +0200

    bazdmeg

--- lib/model/clubs.dart
+++ lib/model/clubs.dart
@@ -4,9 +4,9 @@ import 'package:partner_finder/model/competition.dart';
 import 'package:partner_finder/model/user.dart';
 
 class Clubs {
-  String _location;
-  List<User> _members;
-  List<Competition> _competitions;
-  User _owner;
-  Clubs(this._competitions, this._location, this._members, this._owner);
+  final String location;
+  final List<User> members;
+  final List<Competition> competitions;
+  final User owner;
+  Clubs(this.competitions, this.location, this.members, this.owner);
 }
