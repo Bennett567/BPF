6a53006d46769eade698fd4f48ea16b3847ff51e
Bennett567 <dani.bohm1234@gmail.com>
2020-07-02 15:38:37 +0200

    user props

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -16,7 +16,7 @@ class User extends Equatable {
 
   @override
   // TODO: implement props
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [displayName, email, ownerOfClub];
   @override
   bool get stringify => true;
 }
