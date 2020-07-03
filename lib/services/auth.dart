d958e33ac6ff869c8b166393ea2e8c888e9bc05c
Bennett567 <dani.bohm1234@gmail.com>
2020-07-03 11:09:39 +0200

    save user data in firebase AGAIN

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:partner_finder/model/user.dart';
 import 'package:partner_finder/services/signInInterface.dart';
@@ -7,6 +8,7 @@ class GoogleSignInService implements SignIn {
   final GoogleSignIn _googleSignIn;
   final FirebaseAuth _auth;
   GoogleSignInService(this._googleSignIn, this._auth);
+  final Firestore _db = Firestore.instance;
 
   @override
   Future<FirebaseUser> handleSignIn() async {
@@ -22,6 +24,19 @@ class GoogleSignInService implements SignIn {
           idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
       user = (await _auth.signInWithCredential(credential)).user;
     }
+    updateUserData(user);
     return user;
+  }
+
+  void updateUserData(FirebaseUser user) async {
+    DocumentReference ref = _db.collection('users').document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'email': user.email,
+      'photoURL': user.photoUrl,
+      'displayName': user.displayName,
+      'lastSeen': DateTime.now(),
+    }, merge: true);
   }
 }
